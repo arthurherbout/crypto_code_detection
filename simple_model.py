2fe0342f7540eaaa8ed0952cba93d67ba2be2d01
Hadrien-Cornier <hadrien.cornier@gmail.com>
2019-09-26 14:19:44 -0400

    added 'prime'

--- simple_model.py
+++ simple_model.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     sys.stdout.write("Please provide name of .txt file for the code :")
     file_location = input()
     crypto_file=build_list_of_lines(file_location)
-    DD,N,CC,key,encrypt,decrypt=0,0,0,0,0,0
+    DD,N,CC,key,encrypt,decrypt,prime=0,0,0,0,0,0,0
     for line in crypto_file :
         if '>>' in line : DD+=1
         if '^' in line : N+=1
@@ -29,9 +29,10 @@ if __name__ == '__main__':
         if 'key' in line : key+=1
         if 'encrypt' in line : encrypt+=1
         if 'decrypt'  in line : decrypt +=1
+        if 'prime' in line : prime +=1
     
-    a=['number of >>','number of ^','number of <<','number of mentions of key','number of mentions of encrypt','number of mentions of decrypt']
-    b=[DD,N,CC,key,encrypt,decrypt]
+    a=['number of >>','number of ^','number of <<','number of mentions of key','number of mentions of encrypt','number of mentions of decrypt','number of mentions of prime']
+    b=[DD,N,CC,key,encrypt,decrypt,prime]
     res=list(zip(a,b))
     for i in res : 
         sys.stdout.write(str(i[0])+" "+str(i[1]))
