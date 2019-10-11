1877e4c5e19f95f5dab213e4d6768ee66a57973e
corentinllorca <corentin.llorca@gmail.com>
2019-10-11 15:16:02 -0400

    final json with dataset + final version of combine_json script

--- data/combine_json.py
+++ data/combine_json.py
@@ -3,12 +3,13 @@ import pandas as pd
 BASE_DIR = "C:/Users/coren/columbia/capstone/data/"
 JSON_OUTPUT = "C:/Users/coren/columbia/capstone/data/full_data.json"
 
-#crypto_library_df = pd.read_json(BASE_DIR + "crypto-library/crypto_library_data.json")
+crypto_library_df = pd.read_json(BASE_DIR + "crypto-library/crypto_library_data.json")
 crypto_competitions_df = pd.read_json(BASE_DIR + "crypto_competitions/crypto_competitions_data.json")
 code_jam_df = pd.read_json(BASE_DIR + "code-jam/code-jam_data.json")
 others_df = pd.read_json(BASE_DIR + "others/others_data.json")
 
-full_df = pd.concat([crypto_competitions_df, code_jam_df, others_df], 
+full_df = pd.concat([crypto_library_df, crypto_competitions_df, 
+                     code_jam_df, others_df], 
                     ignore_index=True)
 
 def transform_df(df): 
@@ -16,8 +17,8 @@ def transform_df(df):
     which is file_name and content as two separate columns instead of being
     unites under data"""
     df['file_name'] = [df.data[i]['file_name'] for i in range(df.shape[0])]
-    df['content'] = [df.data[i]['content'] for i in range(df.shape[0])]
     df['is_header'] = [df.file_name[i].split('.')[-1][0] == 'h' for i in range(df.shape[0])]
+    df['content'] = [df.data[i]['content'] for i in range(df.shape[0])]
     df.drop('data', axis=1, inplace=True)
     return(df)
     
