33f7a608bf2c8041603de9b6610eaaf30773eb76
Redouane Dziri <rd2853@columbia.edu>
2019-10-17 17:04:58 -0400

    create train test split

---
+++ data/create_train_test.py
@@ -0,0 +1,40 @@
+import os
+import json
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+from git_root import git_root
+
+BASE_DIR = os.path.join(git_root(), "data")
+json_path = os.path.join(BASE_DIR, "full_data.json")
+
+df = pd.read_json(
+    json_path
+)
+
+train, test, _, _ = train_test_split(
+    df,
+    df.loc[:, "data_source"],
+    test_size = .15,
+    stratify= df.loc[:, "data_source"]
+)
+
+train_source_counts = train["data_source"].value_counts()
+test_source_counts = test["data_source"].value_counts()
+
+for t in ("train", "test"):
+    if t == "train":
+        props = train_source_counts / train.shape[0]
+    else:
+        props = test_source_counts / test.shape[0]
+    print(f"""
+The proportions of files from each source in {t} is:
+{props.round(3)}
+    """)
+
+train_path = os.path.join(BASE_DIR, "train.json")
+test_path = os.path.join(BASE_DIR, "test.json")
+
+train.to_json(train_path, orient='records')
+test.to_json(test_path, orient='records')
