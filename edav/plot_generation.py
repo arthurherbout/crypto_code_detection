55119552dd2917752ff84c9796efee59c3d4dcd6
corentinllorca <corentin.llorca@gmail.com>
2019-10-21 11:13:04 -0400

    Adding some data viz

--- edav/plot_generation.py
+++ edav/plot_generation.py
@@ -2,6 +2,8 @@ import os
 from functools import partial
 import pandas as pd
 from git_root import git_root
+import re
+import matplotlib.pyplot as plt
 
 BASE_DIR = os.path.join(git_root(), "feature_engineering")
 DATA_DIR = os.path.join(git_root(), "data")
@@ -11,11 +13,23 @@ df = pd.read_json(os.path.join(DATA_DIR, "full_data.json"))
 content = df["content"]
 df = df.drop(columns=["content"])
 
+# De-noise by removing comments 
+
+def remove_comments(string):
+    # remove all occurrences streamed comments (/*COMMENT */) from string
+    string = re.sub(re.compile(r"/\*.*?\*/",re.DOTALL ) , "" , string) 
+    # remove all occurrence single-line comments (//COMMENT\n ) from string
+    string = re.sub(re.compile(r"//.*?\n" ) , "" , string)
+    return string
+
+content = content.apply(remove_comments)
+
 feature_dict = {
     "proxy_line": "\n",
     "proxy_comment": "//",
     "proxy_multiline_comment": "/*",
     "proxy_int": "int",
+    "proxy_long": "long",
     "proxy_while_loops": "while",
     "proxy_for_loops": "for",
     "proxy_include": "#include",
@@ -47,8 +61,7 @@ df = df.assign(**count_features)
 loops = ["proxy_while_loops_count", "proxy_for_loops_count"]
 bitwise_ops = ["proxy_bit_left_shift_count", "proxy_bit_right_shift_count", 
                "proxy_bitwise_and_count", "proxy_complement_count",
-               "proxy_bitwise_xor_count", "proxy_xor_count", 
-               "proxy_hexadecimal_count"]
+               "proxy_bitwise_xor_count", "proxy_xor_count"]
 df = df.assign(
     proxy_loops_count = sum(df[col] for col in loops),
     proxy_bitwise_count = sum(df[col] for col in bitwise_ops)
@@ -67,34 +80,31 @@ df = df.assign(
 ## -> Possibly facet that by distinction (which ones? SEE BELOW) 
    # by header vs. non header, important
    # crypto vs. non-crypto
-# Plotting the number of comments and multiline comments (+ distribution)
-   # mmkay why not but don't know how useful (maybe skip for now?)
+   
+## All of the above: can be done by simple mean/median calls
+   
+## All below: can be done on the same graph
+   
 # Plotting int counts for crypto vs non crypto (distribution?)
    # aggregated is enough (total sum per class or sth)
    # also do for `long`, can be informative maybe
 # Plotting loops counts for crypto vs non crypto 
    # aggregated is enough
 # Plotting bitwise counts for crypto vs non crypto (distribution?)
-# Plotting top bitwise operations per category
-   # don't really see why this is particularly interesting, maybe skip for now?
 # Plotting number of includes for crypto vs non crypto 
    # yes!
-# Give top10 include in general case, in crypto case and in non crypto case (have to code that)
-   # sounds interesting
+   
+fil = [5, 6, 9, 16, 17, 18] # filter that only keeps interesting stats
 
-   # For most of these remove comments first to de-noise counts, using:
+# This is a one liner for grouped barplot for labels 0 and 1
+ax = pd.concat([df[df["label"] == 0].mean()[fil],df[df["label"] == 1].mean()[fil]], axis=1).plot.bar()
+ax.set_xticklabels(["int", "long", "include", "hexadecimal", "loops", "bitwise ops"])
+plt.legend(title="Crypto label")
 
-def remove_comments(string):
-    # remove all occurrences streamed comments (/*COMMENT */) from string
-    string = re.sub(re.compile(r"/\*.*?\*/",re.DOTALL ) , "" , string) 
-    # remove all occurrence single-line comments (//COMMENT\n ) from string
-    string = re.sub(re.compile(r"//.*?\n" ) , "" , string)
-    return string
 
-content_without_comments = content.apply(remove_comments)
 
-   # maybe interesting to plot something else than counts for some of these
-   # since counts are strongly correlated with the file size (which will be very unequal across files)
-   # would be nice to have:
-   #  - binary feature if there is at least one bitwise operation or not in the file (and plot number of file per class that has bitwise ops)
-   #  - binary feature if there is at least one long or not in the file (and plot number of file per class that has longs)
+# maybe interesting to plot something else than counts for some of these
+# since counts are strongly correlated with the file size (which will be very unequal across files)
+# would be nice to have:
+#  - binary feature if there is at least one bitwise operation or not in the file (and plot number of file per class that has bitwise ops)
+#  - binary feature if there is at least one long or not in the file (and plot number of file per class that has longs)
